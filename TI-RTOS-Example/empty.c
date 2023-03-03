2149443ba1349fd3ac1e74201a3fdcebb4ee591b
Derek Gilmartin <gilmartd7@gmail.com>
2023-03-03 09:16:20 -0800

    Idle task GPIO
    Associates the Idle task with the green LED

--- TI-RTOS-Example/empty.c
+++ TI-RTOS-Example/empty.c
@@ -35,11 +35,14 @@
  */
 /* XDCtools Header files */
 #include <xdc/std.h>
-#include <xdc/cfg/global.h>
 #include <xdc/runtime/System.h>
 
 /* BIOS Header files */
 #include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/hal/Core.h>
+#include <ti/sysbios/hal/Hwi.h>
+#include <ti/sysbios/knl/Idle.h>
 
 /* TI-RTOS Header files */
 #include <ti/drivers/GPIO.h>
@@ -53,16 +56,72 @@
 /* Board Header file */
 #include "Board.h"
 
+#define TASKSTACKSIZE   512
+
+Task_Struct task1Struct, task2Struct, task3Struct, task4Struct, task5Struct, task6Struct;
+Char task1Stack[TASKSTACKSIZE], task2Stack[TASKSTACKSIZE], task3Stack[TASKSTACKSIZE],
+task4Stack[TASKSTACKSIZE], task5Stack[TASKSTACKSIZE], task6Stack[TASKSTACKSIZE];
+
+Task_Handle task1Handle, task2Handle, task3Handle, task4Handle, task5Handle, task6Handle;
+
+Void Idle_blink(UArg arg0, UArg arg1)
+{
+        GPIO_toggle(Board_GREEN);
+        GPIO_toggle(Board_GREEN);
+
+}
 /*
- *  ======== heartBeatFxn ========
+ *  ======== blinky1 ========
  *  Toggle the Board_LED0. The Task_sleep is determined by arg0 which
  *  is configured for the heartBeat Task instance.
  */
-Void heartBeatFxn(UArg arg0, UArg arg1)
+Void blinky1(UArg arg0, UArg arg1)
 {
     while (1) {
         Task_sleep((UInt)arg0);
-        GPIO_toggle(Board_LED0);
+        GPIO_toggle(Board_PC6);
+    }
+}
+
+Void blinky2(UArg arg0, UArg arg1)
+{
+    while (1) {
+        Task_sleep((UInt)arg0);
+        GPIO_toggle(Board_PC7);
+    }
+}
+
+Void blinky3(UArg arg0, UArg arg1)
+{
+    while (1) {
+        Task_sleep((UInt)arg0);
+
+        GPIO_toggle(Board_PC5);
+    }
+}
+
+Void blinky4(UArg arg0, UArg arg1)
+{
+    while (1) {
+        Task_sleep((UInt)arg0);
+        GPIO_toggle(Board_PB3);
+    }
+}
+
+Void blinky5(UArg arg0, UArg arg1)
+{
+    while (1) {
+        Task_sleep((UInt)arg0);
+        GPIO_toggle(Board_PC4);
+    }
+}
+
+Void blinky6(UArg arg0, UArg arg1)
+{
+    while (1) {
+
+        Task_sleep((UInt)arg0);
+        GPIO_toggle(Board_RED);
     }
 }
 
@@ -71,6 +130,8 @@ Void heartBeatFxn(UArg arg0, UArg arg1)
  */
 int main(void)
 {
+    Task_Params taskParams;
+
     /* Call board init functions */
     Board_initGeneral();
     Board_initGPIO();
@@ -82,11 +143,44 @@ int main(void)
     // Board_initWatchdog();
     // Board_initWiFi();
 
-    /* Turn on user LED */
-    GPIO_write(Board_LED0, Board_LED_ON);
+    Task_Params_init(&taskParams);
+    taskParams.arg0 = 1000;
+    taskParams.stackSize = TASKSTACKSIZE;
+    taskParams.stack = &task1Stack;
+    taskParams.priority = 1;
+    Task_construct(&task1Struct, (Task_FuncPtr)blinky1, &taskParams, NULL);
+    task1Handle = Task_handle(&task1Struct);
 
-    System_printf("Starting the example\nSystem provider is set to SysMin. "
-                  "Halt the target to view any SysMin contents in ROV.\n");
+    taskParams.stack = &task2Stack;
+    taskParams.priority = 2;
+    taskParams.arg0 = 1000;
+    Task_construct(&task2Struct, (Task_FuncPtr)blinky2, &taskParams, NULL);
+    task2Handle = Task_handle(&task2Struct);
+
+    taskParams.stack = &task3Stack;
+    taskParams.priority = 3;
+    taskParams.arg0 = 1000;
+    Task_construct(&task3Struct, (Task_FuncPtr)blinky3, &taskParams, NULL);
+    task3Handle = Task_handle(&task3Struct);
+
+    taskParams.stack = &task4Stack;
+    taskParams.priority = 4;
+    taskParams.arg0 = 1000;
+    Task_construct(&task4Struct, (Task_FuncPtr)blinky4, &taskParams, NULL);
+    task3Handle = Task_handle(&task4Struct);
+
+    taskParams.stack = &task5Stack;
+    taskParams.priority = 5;
+    taskParams.arg0 = 1000;
+    Task_construct(&task5Struct, (Task_FuncPtr)blinky5, &taskParams, NULL);
+    task3Handle = Task_handle(&task5Struct);
+
+    taskParams.stack = &task6Stack;
+    taskParams.priority = 6;
+    taskParams.arg0 = 1000;
+    Task_construct(&task6Struct, (Task_FuncPtr)blinky6, &taskParams, NULL);
+    task3Handle = Task_handle(&task6Struct);
+
     /* SysMin will only print to the console when you call flush or exit */
     System_flush();
 
